937ce56c54fb1fbc2a2bcee30941f9d98f5b9dd0
Aman <99051262+Aman96-star@users.noreply.github.com>
2025-11-04 12:25:47 +0530

    Add more programs

---
+++ IT_technologies/Java/Javas/Topic_wise_program/src/arrays_task/Array_operation2.java
@@ -0,0 +1,24 @@
+package arrays_task;
+import java.util.*;
+
+
+public class Array_operation2 {
+
+	public static void main(String[] args) {
+		Scanner ob =new Scanner(System.in);
+		int x =ob.nextInt();
+		int temp =0;
+		int i;
+		int arr[] =new int[x];
+		for( i=0;i<x;i++) {
+			arr[i]=ob.nextInt();
+			}    // [3]  12   45    22
+		for(int j=0;j<arr[i];j++) {
+			temp=j;
+		}
+		System.out.println(temp);
+		// here i have to caluclate  
+		
+	}
+
+}
